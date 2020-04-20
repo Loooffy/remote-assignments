4c4161e841148e664a7eff6289e9f5b079165dbb
Loooffy <Loooffy@gmail.com>
2020-04-20 16:32:08 +0800

    add week2/assignment4

---
+++ Week-2/Assignment-4/change_welcome_message.js
@@ -0,0 +1,15 @@
+function changeText(){
+    document.getElementById("welcome").innerHTML="Have a Good Time!";
+}
+
+function showMenu(){
+    document.getElementById("prompt").style.visibility="visible";
+}
+
+function hideMenu(){
+    document.getElementById("prompt").style.visibility="hidden";
+}
+
+function showMore(){
+    document.getElementById("more").style.display="flex";
+}
